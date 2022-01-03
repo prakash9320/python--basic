6958d44b302d0eeba8403547813202c410c91063
prakash shukla <prakashshukla275@gmail.com>
2022-01-03 14:46:07 +0530

    pizza order practice

--- Day 3/Day 3.py
+++ Day 3/Day 3.py
@@ -8,7 +8,7 @@ if height > 120:
      bill = 5 
      print ("child tickets are $5.")
     elif age <= 18:
-     bill = 7
+     bill = 7  
      print("youth tickets are $7.")
          
     else:
@@ -17,10 +17,8 @@ if height > 120:
 
     wants_photo = input("DO you a photo Taken ? Y or No")
     if wants_photo == "Y":
-        bill = bill +3
+        bill += 3
     print(f"your final bill is $ {bill} ")
-
-
 else:
     print("Sorry ,you have to go taller before you can ride .")
 
@@ -41,3 +39,26 @@ else:
 #     print ("clinically obese")
 # else:
 #  print("please lose Your Weight")
+
+ # =======================================================================
+print("Welcome to the  pizza  Deliveries")
+size = input("what is pizza  do you  want S ,M , OR L")
+add_pepperies = input(" Do you Want pepperies ?")
+extra_cheese = input("Do you want Extra Cheese")
+bill = 0
+if size == 'S':
+ bill += 15
+elif size == 'M':
+ bill += 20
+elif size == 'L' :
+ bill  += 25
+
+if add_pepperies == "Y":
+ if size == "S":
+  bill +=2
+
+if extra_cheese == "Y":
+ bill += 1
+else:
+ bill += 3
+print(f"Your bill is ${bill}")
