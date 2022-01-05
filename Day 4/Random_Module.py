f4fcfbd3ced7ed1e079c096dd1bb3a2ec738d5d0
prakash shukla <prakashshukla275@gmail.com>
2022-01-05 17:06:34 +0530

    generat random number

--- Day 4/Random_Module.py
+++ Day 4/Random_Module.py
@@ -1,6 +1,11 @@
 import random
-import my_Module
+# import my_Module
 random_integer = random.randint(1,10)
 print(random_integer)
 
-print(my_Module.pi)
+# print(my_Module.pi)
+random_float = random.random() * 5 
+print(random_float)
+
+love_score = random.randint(1,100)
+print(f"Your Love Score is : {love_score}")
