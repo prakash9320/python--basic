9ca7cac6c85c8394962e1ece6814ce025e334157
prakash shukla <prakashshukla275@gmail.com>
2022-01-05 15:00:27 +0530

    add and remove some code

--- Day 3/Treasure Island.py
+++ Day 3/Treasure Island.py
@@ -1,7 +1,9 @@
 print("Welcome to Tresure Island")
 print("Your mission is to find the treasure")
 choice1 = input('you `re at the crossroad , where do you want to go ? Type "left" or "right". \n').lower()
+
 if choice1 == "left":
-   input('You` ve  come to the lake There is island in the middle of the lake . Type "wait" to wait for the boat . Type to "swim" to cross .\n')
+    # continue in the game 
+ print("")
 else:
-    print("")
+ print("You fell into a hole.Game Over")
