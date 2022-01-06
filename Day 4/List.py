b16c9b10cec46ff2a467391893cc760579b0d077
prakash shukla <prakashshukla275@gmail.com>
2022-01-06 12:51:48 +0530

    basic python list practice

---
+++ Day 4/List.py
@@ -0,0 +1,8 @@
+ # Declare the python List
+state_of_america = ["Delware", "pennsylvania" ,"New York", "Georgia","North Carolina"]
+
+ # print the first Name of  City
+print(state_of_america[0])
+
+# print last Name of city
+print(state_of_america[-1])
