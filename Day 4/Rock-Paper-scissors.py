1dd26bfdfab64efc18de0583405384f1cca088b2
prakash9320 <prakashshukla275@gmail.com>
2022-01-08 14:29:43 +0530

    add some conditions

--- Day 4/Rock-Paper-scissors.py
+++ Day 4/Rock-Paper-scissors.py
@@ -7,3 +7,8 @@ user_choice = input("what do you choose ? Type 0 for rock , 1 for paper and 2 fo
 
 computer_choice = random.randint(0,2)
 print(f"Computer chose {computer_choice}")
+
+if user_choice == 0 and computer_choice  == 2 :
+    print("User Win")
+elif computer_choice  >user_choice:
+ print("Computer Win !")
