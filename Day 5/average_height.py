a4a6fe4f98a46fc27c55695d1b2d6a3cfb7b8644
prakash9320 <prakashshukla275@gmail.com>
2022-01-10 15:43:55 +0530

    add some code

---
+++ Day 5/average_height.py
@@ -0,0 +1,15 @@
+student_height = input("Input a List of student height").split()
+for n in range(0 ,len(student_height)):
+    student_height[n] = int(student_height[n])
+    print(student_height)
+
+    total_height = 0
+    for height in student_height :
+     total_height += height
+     print(total_height)
+ 
+
+
+    number_of_student = len(student_height)
+    average_height = round(total_height / number_of_student)
+    print(average_height)
