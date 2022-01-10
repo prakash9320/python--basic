7c669612e67d8e65692ad4c44235f8cd6b610876
prakash9320 <prakashshukla275@gmail.com>
2022-01-10 16:05:46 +0530

    check the heghest number

--- Day 5/High Score.py
+++ Day 5/High Score.py
@@ -2,3 +2,5 @@ student_score = input("Input  a List of student score").split()
 for n in range(0,len(student_score)) :
     student_score[n] = int(student_score[n])
     print(student_score)
+
+    print(max(student_score))
