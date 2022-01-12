7a5632508797a2c5447a2edd29007ce2c5303ebd
prakash9320 <prakashshukla275@gmail.com>
2022-01-12 15:16:49 +0530

    generate random password

---
+++ Day 5/password-generator.py
@@ -0,0 +1,20 @@
+# Password Generator Project
+import random
+letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n' ,'o','p','q','r','s','t','u','v','w','x','y','z'
+           'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'   ]
+number = ['0','1','2','3','4','5','6','7','8','9']
+
+symbol = ['!','#','$','%','&','(',')','*','+']
+ 
+print("Welcome to the pyPassword Generator !")
+nr_letters = int(input("how many letters would you like in your password :\n"))
+nr_symbol = int(input("How Many Symbol Would You  Like : \n"))
+nr_number = int(input("How Many Number Would You Like :\n"))
+
+# Eazy Level
+password = ""
+
+for char in range(1,nr_letters + 1):
+    random_char = random.choice(letter)
+    password +=random_char
+print(password)  
