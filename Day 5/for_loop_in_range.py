3609478d69ede2fcd65d0cbaff8a0e0e82a37c22
prakash9320 <prakashshukla275@gmail.com>
2022-01-12 14:10:00 +0530

    add exrcise

--- Day 5/for_loop_in_range.py
+++ Day 5/for_loop_in_range.py
@@ -1,3 +1,24 @@
 # fot loop in range 
-for number in range(1,10,3):
- print(number)    
+
+# for number in range(1,11,3):
+#  print(number)    
+
+
+# ============
+
+# total = 0
+# for number in range(1,101):
+#     total += number
+#     print(total)
+
+# =======================
+ # exrcise
+for number in range(1,101):
+    if number % 3 == 0 and number % 5 == 0 :
+      print("FizzBuss ")
+    elif number % 3 == 0 :
+     print("FizzBuzz")
+    elif number % 5 == 5:
+     print("FizzBuzz") 
+    else :
+        print(number) 
