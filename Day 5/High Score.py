9efac62e7040d6678c4c1f98667435bdb446913d
prakash9320 <prakashshukla275@gmail.com>
2022-01-12 12:31:15 +0530

    add some code

--- Day 5/High Score.py
+++ Day 5/High Score.py
@@ -3,4 +3,10 @@ for n in range(0,len(student_score)) :
     student_score[n] = int(student_score[n])
     print(student_score)
 
-    print(max(student_score))
+    print(max(student_score))   
+
+    highest_Score = 0
+    for score in student_score :
+        if score > highest_Score:
+            highest_Score = score
+    print(f"The highest score in the class is : {highest_Score}")        
