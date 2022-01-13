ac6d332cf475bcb8dc6c96378cf07a2e9dd88018
prakash9320 <prakashshukla275@gmail.com>
2022-01-13 16:11:00 +0530

    add for loop and while loop example

---
+++ Day 6/While_Loop.py
@@ -0,0 +1,19 @@
+# Example
+
+# This code are only example 
+
+# def jump():
+#     move() 
+#     move()
+#     move()
+ 
+ # This Is only Example 
+
+# number_of_hurdles = 6 
+# while number_of_hurdles > 0:
+#  jump()
+#  number_of_hurdles -=1
+#  print(number_of_hurdles)
+
+# while not at_goal():
+#     jump()
