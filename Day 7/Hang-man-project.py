198999c12cd8512b1e10d9340091d0d808214c6a
prakash9320 <prakashshukla275@gmail.com>
2022-01-14 16:38:59 +0530

    chose a word

---
+++ Day 7/Hang-man-project.py
@@ -0,0 +1,23 @@
+import random
+work_list = ["ardvark","baboon","camel"]
+
+# TODO-1 randomely choose a word from word_list and assign 
+#  it to variable  called chosem_word
+     
+
+chosen_word = random.choice(work_list)
+ # Testing Code
+print("passt , the solution  is :"+ chosen_word)
+
+# TODO-2 Aske the user to guess the letter and assign thair
+#      answer to variable called guess .Make guess 
+#      lowercase 
+guess = input("Enter a word")
+print(guess)
+# TODO-3 CHECK IF THE LATTER THE USER GUESSESD  (GUESSE)
+#  IS ONE OF THE LETTER  IN THE CHOOSE_WORD
+for letter in work_list:
+    if letter == guess:
+       print("Right")
+    else:
+       print("Wrong")  
