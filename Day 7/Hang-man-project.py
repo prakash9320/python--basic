73cc66a42c8ad0874178b4a8e398020d3208cf8a
prakash9320 <prakashshukla275@gmail.com>
2022-01-18 11:59:46 +0530

    nothing

--- Day 7/Hang-man-project.py
+++ Day 7/Hang-man-project.py
@@ -1,7 +1,7 @@
 import random
 work_list = ["ardvark","baboon","camel"]
 
-# TODO-1 randomely choose a word from word_list and assign 
+#     TODO-1 randomely choose a word from word_list and assign 
 #  it to variable  called chosem_word
      
 
@@ -17,12 +17,14 @@ display = []
 for _ in range(len(chosen_word)):
    display += "_"
    print(display)
-guess = input("Enter a word")
-print(guess)
+   end_of_game =False
+   while end_of_game:
+      guess = input("guess a letter").lower()
+      print(guess)
 # TODO-3 CHECK IF THE LATTER THE USER GUESSESD  (GUESSE)
 #  IS ONE OF THE LETTER  IN THE CHOOSE_WORD
-for letter in work_list:
-    if letter == guess:
-       print("Right")
-    else:
+      for position in range(len(chosen_word)) :
+       if position == guess:
+        print("Right")
+      else:
        print("Wrong")  
